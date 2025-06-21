388d9f70280c51496f2e78659e53a7f24118cb8c
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-06-21 20:44:18 +0530

    Replaced font URLs

--- app/resume/[company]/utils/font.ts
+++ app/resume/[company]/utils/font.ts
@@ -4,15 +4,15 @@ Font.register({
   family: "Open Sans",
   fonts: [
     {
-      src: "http://fonts.gstatic.com/s/opensans/v13/IgZJs4-7SA1XX_edsoXWog.ttf",
+      src: "https://fonts.gstatic.com/s/opensans/v13/IgZJs4-7SA1XX_edsoXWog.ttf",
       fontWeight: 400,
     },
     {
-      src: "http://fonts.gstatic.com/s/opensans/v13/MTP_ySUJH_bn48VBG8sNSi3USBnSvpkopQaUR-2r7iU.ttf",
+      src: "https://fonts.gstatic.com/s/opensans/v13/MTP_ySUJH_bn48VBG8sNSi3USBnSvpkopQaUR-2r7iU.ttf",
       fontWeight: 600,
     },
     {
-      src: "http://fonts.gstatic.com/s/opensans/v13/k3k702ZOKiLJc3WVjuplzC3USBnSvpkopQaUR-2r7iU.ttf",
+      src: "https://fonts.gstatic.com/s/opensans/v13/k3k702ZOKiLJc3WVjuplzC3USBnSvpkopQaUR-2r7iU.ttf",
       fontWeight: 700,
     },
   ],
